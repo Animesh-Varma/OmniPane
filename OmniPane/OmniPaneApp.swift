75400e81cee9224bb1a5ae330ea1b8cf27be5b30
Aishwarya <aishwaryarana2807@gmail.com>
2026-04-08 09:47:21 +0530

    Initial Commit

---
+++ OmniPane/OmniPaneApp.swift
@@ -0,0 +1,17 @@
+//
+//  OmniPaneApp.swift
+//  OmniPane
+//
+//  Created by Aishwarya Rana on 08/04/26.
+//
+
+import SwiftUI
+
+@main
+struct OmniPaneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
